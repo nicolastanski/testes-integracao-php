dbb6dcb327bd1901b80922444aefd95f390d8488
Nicolas Tanski Pavão <nicolas.tanski@gmail.com>
2020-11-14 09:40:11 -0300

    Delete data

---
+++ tests/Integration/Dao/LeilaoDaoTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alura\Leilao\Tests\Integration\Dao;
+
+use Alura\Leilao\Dao\Leilao as LeilaoDao;
+use Alura\Leilao\Infra\ConnectionCreator;
+use Alura\Leilao\Model\Leilao;
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertContainsOnlyInstancesOf;
+use function PHPUnit\Framework\assertInstanceOf;
+
+class LeilaoDaoTest extends TestCase
+{
+    private $pdo;
+
+    public function setUp(): void
+    {
+        $this->pdo = ConnectionCreator::getConnection();
+    }
+
+    public function testInsercaoEBuscaDevemFuncionar()
+    {
+        // arrange
+        $leilao = new Leilao('Variante 0KM');
+        $pdo = ConnectionCreator::getConnection();
+        $leilaoDao = new LeilaoDao($this->pdo);
+        $leilaoDao->salva($leilao);
+
+        // act
+        $leiloes = $leilaoDao->recuperarNaoFinalizados();
+
+        // asserts
+        self::assertCount(1, $leiloes);
+        self::assertContainsOnlyInstancesOf(Leilao::class, $leiloes);
+        self::assertSame('Variante 0KM', $leiloes[0]->recuperarDescricao());
+    }
+
+    public function tearDown(): void
+    {
+        $this->pdo->exec('DELETE FROM leiloes;');
+    }
+}
